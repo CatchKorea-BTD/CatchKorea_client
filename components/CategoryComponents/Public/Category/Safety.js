deb8ae037ef0c0d865cc370277e6181a86d7aa9b
hansanga <cab08132@naver.com>
2023-08-18 08:56:25 +0900

    api 수정

--- components/CategoryComponents/Public/Category/Safety.js
+++ components/CategoryComponents/Public/Category/Safety.js
@@ -9,7 +9,7 @@ export default function Content() {
         // API에서 메뉴 데이터를 가져오는 함수
         const fetchMenuData = async () => {
             try {
-                const response = await axios.get('https://catchkorea-a5799a624288.herokuapp.com/post/{category_id}'); // API 엔드포인트
+                const response = await axios.get('https://catchkorea-a5799a624288.herokuapp.com/post/24'); // API 엔드포인트
                 setMenuData(response.data); // API에서 받아온 데이터를 상태에 저장
             } catch (error) {
                 console.error('Error fetching menu data:', error);
@@ -23,11 +23,11 @@ export default function Content() {
         <>
             {menuData.map(menu => (
                 <MenuBox
-                    key={menu.link} // 각 메뉴는 고유한 키값을 가져야 합니다.
-                    link={menu.link}
-                    iconSrc={menu.iconSrc}
-                    text1={menu.text1}
-                    text2={menu.text2}
+                    key={menu.id} // 각 메뉴는 고유한 키값을 가져야 합니다.
+                    link='www.com'
+                    iconSrc={menu.imageLink}
+                    text1={menu.title}
+                    text2={menu.contents}
                     hashtag1={menu.hashtag}
                 />
             ))}
