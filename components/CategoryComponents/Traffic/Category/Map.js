08b14015da8a3959f642ad5c400bed4a8ac34c51
hansanga <cab08132@naver.com>
2023-08-18 09:02:00 +0900

    api 수정

--- components/CategoryComponents/Traffic/Category/Map.js
+++ components/CategoryComponents/Traffic/Category/Map.js
@@ -3,26 +3,35 @@ import Link from 'next/link'
 import MenuBox from '../../../../components/AppComponents/MenuBox';
 
 export default function Content() {
+    const [menuData, setMenuData] = useState([]);
+
+    useEffect(() => {
+        // 백엔드 API에서 메뉴 데이터를 가져오는 함수를 호출하고 데이터를 설정합니다.
+        const fetchMenuData = async () => {
+            try {
+                const response = await fetch('https://catchkorea-a5799a624288.herokuapp.com/post/34');
+                const data = await response.json();
+                setMenuData(data);
+            } catch (error) {
+                console.error('Error fetching menu data:', error);
+            }
+        };
+        fetchMenuData();
+    }, []);
+
     return (
-        <> 
-        <MenuBox 
-            link = "/InfoComponents/Traffic/Map/NaverMap" 
-            iconSrc = '\AppIcon\NaverMap.png' 
-            text1 = 'Naver Map' 
-            text2 = 'The most popular Korean subway application' 
-            hashtag1='#traffic'
-            hashtag2=''
-            hashtag3 ='' 
-        /> 
-        <MenuBox
-            link="/InfoComponents/Traffic/Map/KakaoMap"
-            iconSrc='\AppIcon\KakaoMap.png'
-            text1='KaKao Map'
-            text2='The most popular Korean Bus application'
-            hashtag1='#traffic'
-            hashtag2=''
-            hashtag3 =''
-        />
-    </>
+        <>
+            {menuData.map((menu, index) => (
+                <MenuBox
+                    key={index}
+                    link="/InfoComponents/Traffic/Map/KakaoMap"
+                    iconSrc={menu.imageLink}
+                    text1={menu.title}
+                    text2={menu.contents}
+                    hashtag1={menu.hashtag}
+                />
+            ))}
+        </>
     );
-};
+}
+
